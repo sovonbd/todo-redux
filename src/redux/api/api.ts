b8df562ea6cbebda9876e1244a588ab11fd97d21
MR. Sovon <sovonsdp@gmail.com>
2024-08-03 20:50:00 -0500

    updated

--- src/redux/api/api.ts
+++ src/redux/api/api.ts
@@ -1,9 +1,16 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const baseApi = createApi({
+export const baseApi = createApi({
   reducerPath: "baseApi",
   baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000" }),
-  endpoints: (builder) =>({
-    
-  })
+  endpoints: (builder) => ({
+    getTodos: builder.query({
+      query: () => ({
+        url: "/tasks", // task is the api endpoint here
+        method: "GET",
+      }),
+    }),
+  }),
 });
+
+export const { useGetTodosQuery } = baseApi;
